f7dd290de0647e2b441103233ea6e1f19afeb427
Francisco Bruno <francisco.bruno@milleniumbcp.pt>
2025-07-16 16:48:51 +0100

    added comment sections

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,10 +1,23 @@
 import { fetchComments } from "./api";
 import "./PostDetail.css";
+import { useQuery } from "@tanstack/react-query";
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["comments", post.id],
+    queryFn: () => fetchComments(post.id),
+  });
 
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (isError) {
+    return <h3>Error loading comments</h3>;
+  }
+  if (!data) {
+    return <h3>No comments found</h3>;
+  }
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
